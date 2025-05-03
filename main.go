50e691957b5b691722a4fe21f60bdb7fe029f369
rickm2618 <93356570+rickm2618@users.noreply.github.com>
2025-05-03 17:51:32 -0500

    Update main.go

--- main.go
+++ main.go
@@ -40,14 +40,6 @@ func showAd() {
 	message := fmt.Sprintf("%s.", lred.Sprint("Evilginx Seriously Modded"))
 	log.Info("%s", message)
 }
-func showAd() {
-	lred := color.New(color.FgHiRed)
-	lyellow := color.New(color.FgHiYellow)
-	white := color.New(color.FgHiWhite)
-	message := fmt.Sprintf("%s: %s %s", lred.Sprint("Evilginx Mastery Course"), lyellow.Sprint("https://academy.breakdev.org/evilginx-mastery"), white.Sprint("(learn how to create phishlets)"))
-	log.Info("%s", message)
-}
-
 var google_bypass = flag.Bool("google-bypass", false, "Enable Google Bypass")
 func init() {
 	flag.Parse()
